a65baa3ae1e98030ff1494576917918252d11050
Hrithik2212 <hr6466@srmist.edu.in>
2023-09-23 08:07:54 +0530

    Added particpants

--- studybud/base/views.py
+++ studybud/base/views.py
@@ -109,15 +109,16 @@ def home(request):
 def room(request , pk ):
     room = Room.objects.get(id=pk) 
     messages = room.message_set.all().order_by('created')
-
+    participants = room.participants.all()
     if request.method == 'POST' :
         message =  Message.objects.create(
             user = request.user , 
             room = room , 
             body = request.POST.get('body')
         )
+        room.participants.add(request.user)
         return redirect('room' , pk=room.id )
-    context = {'room' : room , 'messages':messages }
+    context = {'room' : room , 'messages':messages , 'participants':participants }
     return render(request , 'base/room.html' ,context )
 
 
