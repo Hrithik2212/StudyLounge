7c6ae578f64d3a83599b6cea8ba92d65107e2574
Hrithik2212 <hr6466@srmist.edu.in>
2023-09-24 13:47:34 +0530

    added static files and modified room form

--- studybud/base/forms.py
+++ studybud/base/forms.py
@@ -5,6 +5,7 @@ class RoomForm(ModelForm):
     class Meta:
         model = Room 
         fields = '__all__'
+        exclude = ['participants' , 'host']
 
 class UserForm(ModelForm):
     class Meta:
